d70b8d171f308b72b783da95bb27320ed42b1a22
unknown <jonianttila89@hotmail.com>
2022-11-17 15:09:00 +0200

    from second week

--- week2/ExpressJS/app.js
+++ week2/ExpressJS/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.get("/store", (req, res) => {
+  res.send("Welcome to Joni's store");
+});
 app.get("/store/fruit", (req, res) => {
   res.json([{ name: "banana", price: 3 }]);
 });
@@ -11,7 +15,7 @@ app.get("/store/vegetables", (req, res) => {
   ]);
 });
 app.get("/", (req, res) => {
-  res.redirect("/store/fruit");
+  res.redirect("/store");
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
